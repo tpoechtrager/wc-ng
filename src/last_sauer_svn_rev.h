3e4a32aef5856cc64295340ced02e0137f9e7245
Thomas Pöchtrager <t.poechtrager@gmail.com>
2015-07-11 17:55:10 +0200

    merge r4846 & r4854

--- src/last_sauer_svn_rev.h
+++ src/last_sauer_svn_rev.h
@@ -4,7 +4,3 @@
 // -4957 (add libwinpthread dlls)
 // -4948 (makefile cleanups)
 //
-// -4846 (saner moving/entmoving)              (data folder conflict with std client)
-// -4854 (rename octabrushes to prefabs)       (data folder conflict with std client)
-//
-//
