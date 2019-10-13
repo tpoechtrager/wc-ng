85ed3ae01d7db8b59ded2e82ea1bc75a34149efa
Thomas Pöchtrager <t.poechtrager@gmail.com>
2019-10-13 16:51:37 +0200

    4846, 4854

--- src/last_sauer_svn_rev.h
+++ src/last_sauer_svn_rev.h
@@ -4,7 +4,4 @@
 // -4957 (add libwinpthread dlls)
 // -4948 (makefile cleanups)
 //
-// -4846 (saner moving/entmoving)              (data folder conflict with std client)
-// -4854 (rename octabrushes to prefabs)       (data folder conflict with std client)
-//
 //
