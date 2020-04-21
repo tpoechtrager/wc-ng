e048a92cc1285ab646aca2a887b6b4083fc14a43
Thomas Pöchtrager <t.poechtrager@gmail.com>
2020-04-21 20:16:18 +0200

    Makefile: Update depends + more MacOS fixes

--- src/build/set_version.sh
+++ src/build/set_version.sh
@@ -19,6 +19,14 @@ test $? -ne 0 && echo "git not installed?" 2>&1 && exit 1
 REVISION=$(git log --pretty=format:'%h' -n 1)
 COMMIT_COUNT=$(git rev-list HEAD --count)
 
-cp "$src" "$dst"
-sed -i'' -e "s/__GIT_REVISION__/$REVISION/g" "$dst"
-sed -i'' -e "s/__GIT_COMMIT_COUNT__/${COMMIT_COUNT}/g" "$dst"
+cp "$src" "$dst.new"
+sed -i'' -e "s/__GIT_REVISION__/$REVISION/g" "$dst.new"
+sed -i'' -e "s/__GIT_COMMIT_COUNT__/${COMMIT_COUNT}/g" "$dst.new"
+
+if [ ! -f "$dst" ]; then
+    mv "$dst.new" "$dst"
+elif cmp "$dest.new" "$dst" 2>/dev/null; then
+    mv "$dst.new" "$dst"
+fi
+
+rm -f "$dst.new"
